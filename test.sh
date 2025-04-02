f674a5978c40b5c89e543b9092714f790149997b
Simone Sanfratello <simone.sanfra@gmail.com>
2025-04-02 16:53:59 +0200

    update test script

--- test.sh
+++ test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+while true; do
+
 # Fetch the list of products
 curl http://127.0.0.1:3042/products
 
 # Loop through product IDs from 1 to 100
 for i in {1..100}; do
   curl "http://127.0.0.1:3042/products/$i"
+  sleep 1
+done
+
+sleep 2
+
 done
